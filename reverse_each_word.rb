f60b0f6b2210f39b543a99f91194f762ec6188f0
Jake Mills <jake.mills.05@gmail.com>
2020-07-05 23:57:16 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,8 @@
 require "pry"
 
 def reverse_each_word(sentence)
-  sent_array = []
-  sent_array << sentence
-  sent_array.collect do |words|
+  sentence.split.collect do |words|
     # binding.pry
-    words.reverse
+    for 
   end
 end
